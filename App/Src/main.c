83a2c733533004b29c8ca8318876ee91e57a014d
Anton Chernov <malikalhack@gmail.com>
2026-02-19 00:24:46 +0200

    Added QP framework files

---
+++ App/Src/main.c
@@ -0,0 +1,19 @@
+/**
+ * @file    main.c
+ * @version 0.0.0
+ * @authors Anton Chernov
+ * @date    02/02/2026
+ * @date    @showdate "%m/%d/%Y"
+ */
+
+/********************************* Include files ******************************/
+
+/********************************* Definitions ********************************/
+
+/********************************* Entry point ********************************/
+
+/** @brief  Main entry point of the application. */
+int main(void) {
+    while(1);
+    return 0;
+
