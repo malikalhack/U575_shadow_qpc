977956f8ec85a8cdc19c0f6e6bc9e589768ed0dc
Anton Chernov <malikalhack@gmail.com>
2026-02-22 20:39:38 +0200

    Added QP calls to main function

--- App/Src/main.c
+++ App/Src/main.c
@@ -7,13 +7,27 @@
  */
 
 /********************************* Include files ******************************/
-
+#include "qpc.h"
 /********************************* Definitions ********************************/
+
+Q_DEFINE_THIS_FILE
+
+/****************************** External prototypes ***************************/
+
+extern void task_start(void);
 
 /********************************* Entry point ********************************/
 
 /** @brief  Main entry point of the application. */
 int main(void) {
-    while(1);
-    return 0;
-
+    QF_init();       // initialize the framework and the underlying RT kernel
+    QXK_init();       // initialize the QXK kernel
+#ifdef Q_SPY
+    if (QS_INIT((void *)0) != 0U) { /* initialize the QS software tracing */
+        Q_ERROR();
+    }
+#endif
+    task_start();    // create the AOs and start Threads
+    return QF_run(); // run the QF application
+}
+/******************************************************************************/
